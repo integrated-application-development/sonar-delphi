6612564a9ce6ef4a568d2e3610439b810611d6b3
alecfn <apfor2@student.monash.edu>
2018-07-02 21:56:50 +1000

    Some changes to the ConstantNotationRule. Bit of progress, definitely able to raise violation on constants, but raising it for every symbol under the const declaration. Need to work out how to just look at the string on the left.

--- src/main/java/org/sonar/plugins/delphi/pmd/rules/ConstantNotationRule.java
+++ src/main/java/org/sonar/plugins/delphi/pmd/rules/ConstantNotationRule.java
@@ -15,25 +15,23 @@ public class ConstantNotationRule extends DelphiRule {
     protected void init(){
         super.init();
         sonarMessage = "Constant values should be prepended with C_";
+
     }
 
     @Override
     public void visit(DelphiPMDNode node, RuleContext ctx){
 
         if(node.getType() == DelphiLexer.CONST){
-            Pattern constRegex = Pattern.compile("\bC_");
 
-            // For each value declared under a const declaration, check syntax
-            for(int i = 0; i < node.getChildCount(); ++i){
-                Tree declaredConstVal = node.getChild(i);
+            for(int i = 0; i < node.getChildCount(); i++){
+                // fixme getChildCount gets every single token under a const declaration, so get 25 violations
+                String constName =  node.getChild(i).getText();
 
-                if(!constRegex.matcher(declaredConstVal.getText()).find()){
-                    addViolation(ctx, (DelphiPMDNode) declaredConstVal, sonarMessage);
+
+                if(!constName.startsWith("C_")){
+                    addViolation(ctx, node);
                 }
-
             }
-
-
 
         }
     }
