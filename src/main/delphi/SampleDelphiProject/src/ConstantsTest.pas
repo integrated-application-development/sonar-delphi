a10a31023d679c0278d147c8fa4fb5c7fde5bb63
alecfn <apfor2@student.monash.edu>
2018-07-02 21:07:28 +1000

    Uploading a new sample to use for testing constants. Turns out the scanner was erroring out on the original file because it was missing necessary syntax so the test was never actually working.
    We can check for file read errors in the scanner output.

--- src/main/delphi/SampleDelphiProject/src/ConstantsTest.pas
+++ src/main/delphi/SampleDelphiProject/src/ConstantsTest.pas
@@ -1,6 +1,33 @@
+unit ConstantsTest;
+
+interface
+
+implementation
+
+
 const
-  Name = 'Tao Yue';
-  FirstLetter = 'a';
-  Year = 1997;
-  pi = 3.1415926535897932;
-  UsingNCSAMosaic = TRUE;
+  MAX_LINES = 3;
+  CRUDE_PI = 22 / 7;
+  HELLO = 'Hello World';
+  LETTERS = ['A'..'Z', 'a'..'z'];
+  DECISION = True;
+
+var
+  i : Integer;
+
+begin
+  // Display our crude value of Pi
+  ShowMessage('Crude Pi = ' + FloatToStr(CRUDE_PI));
+
+  // Say hello to the WOrld
+  ShowMessage(HELLO);
+
+  // Display MAX_LINES of data
+  for i := 1 to MAX_LINES do
+  begin
+    // Do some checking - note that Char(i+64) = 'A'
+    if DECISION and (Char(i + 63) in LETTERS)
+    then ShowMessage(Char(i + 63) + ' is a letter')
+    else ShowMessage(Char(i + 63) + ' is not a letter');
+  end;
+end.
