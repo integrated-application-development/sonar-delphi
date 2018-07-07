ef7322245ca3b65e98655179e42cf23540165220
alecfn <apfor2@student.monash.edu>
2018-07-07 18:07:33 +1000

    Adding a batch script for automatically compiling and deploying plugins to a local Sonar instance. Just change the paths as needed.

---
+++ autodeploy.bat
@@ -0,0 +1,14 @@
+@echo off
+REM This will compile, copy and restart the server automatically, change hardcoded paths as appropriate
+
+echo Compiling (clean install)...
+cmd /c "mvn clean install"
+
+echo Copying compiled plugin to Sonar server install...
+
+copy /b/v/y C:\Uni\StudioProject-FIT4002\FIT4002_SonarQube_Delphi\target\sonar-delphi-plugin-0.3.4.jar C:\sonarqube-7.2\extensions\plugins\sonar-delphi-plugin-0.3.4.jar /Y
+
+echo Restarting server...
+
+cmd /c "C:\sonarqube-7.2\bin\windows-x86-64\StopNTService.bat"
+cmd /c "C:\sonarqube-7.2\bin\windows-x86-64\StartNTService.bat"
