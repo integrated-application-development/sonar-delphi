43f3c84d22451edfb7ac9897d94b168cf08d3655
Mayank Adlakha <mayankadlakha@gmail.com>
2018-07-25 15:55:22 +1000

    Still working on this.

--- src/main/java/org/sonar/plugins/delphi/pmd/rules/TooLongLineRule.java
+++ src/main/java/org/sonar/plugins/delphi/pmd/rules/TooLongLineRule.java
@@ -19,24 +19,24 @@ public class TooLongLineRule extends DelphiRule{
         lineNumber = 0;
         lineLength = 0;
         limit = 100;
-
-
     }
 
     @Override
     public void visit(DelphiPMDNode node, RuleContext ctx) {
+
         int nodeTextLength = node.getText().length();
         int nodeLineNumber = node.getLine();
 
         if(nodeLineNumber != lineNumber){
             lineLength = 0;
             lineNumber = nodeLineNumber;
+            lineLength -= 1;
         }
 
-        lineLength += nodeTextLength;
+        lineLength += nodeTextLength + 1;
 
         if(lineLength > limit){
-            String sonarMessage = "Line " + nodeLineNumber + " is too long (" + lineLength + " characters). Maximum character count should be "
+            String sonarMessage = "Line too long (" + lineLength + " characters). Maximum character count should be "
                     + limit + ".";
             addViolation(ctx, node, sonarMessage);
         }
@@ -46,6 +46,7 @@ public class TooLongLineRule extends DelphiRule{
         System.out.print("NodeLength: " + nodeTextLength + "\n");
         System.out.print("LineLength: " + lineLength);
         System.out.print("\n***********\n");
+        System.out.print(node.getAsDocument());
     }
 
 }
