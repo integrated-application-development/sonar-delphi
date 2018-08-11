ef07943d75aff297f3825d0805a7ac0b84a4e403
alecfn <apfor2@student.monash.edu>
2018-08-11 15:14:24 +1000

    Calls to methods which do not have any parameters passed identified.
    Rule seems to be working fine, have set to 'info' severity by default as
    it's a pretty minor issue and easily fixed.

--- src/main/java/org/sonar/plugins/delphi/pmd/rules/EmptyBracketsRule.java
+++ src/main/java/org/sonar/plugins/delphi/pmd/rules/EmptyBracketsRule.java
@@ -6,13 +6,31 @@ import org.sonar.plugins.delphi.antlr.ast.DelphiPMDNode;
 
 public class EmptyBracketsRule extends DelphiRule {
 
+    /**
+     * This rule searches for any calls to methods which include brackets () but do not pass any parameters. This
+     * is not necessary in Delphi so warnings are raised.
+     * @param node the current node
+     * @param ctx the ruleContext to store the violations
+     */
     @Override
     public void visit(DelphiPMDNode node, RuleContext ctx){
 
-        if(node.getType() == DelphiLexer.TkFunctionName){
-            System.out.print("************\n");
-            System.out.print(node.getType() + "\n");
-            System.out.print(node.getText() + "\n");
+        // Look for any method call nodes under a begin block ('TkIdentifier'), check if the next two are parentheses
+        if(node.getType() == DelphiLexer.BEGIN){
+
+            for(int i = 0; i < node.getChildCount() -1; i++) {
+
+                DelphiPMDNode childNode = (DelphiPMDNode) node.getChild(i);
+                if(childNode.getType() == DelphiLexer.TkIdentifier){
+                    if(node.getChild(i + 1).getType() == DelphiLexer.LPAREN){
+                        if(node.getChild(i + 2).getType() == DelphiLexer.RPAREN){
+                            // The next node and the subsequent nodes were parentheses, no arguments passed
+                            addViolation(ctx, childNode);
+                        }
+                    }
+                }
+
+            }
         }
 
     }
