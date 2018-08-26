38879754dc2e7f9b57fdac17fc11dbb1b3e40a2c
croach1337 <davidfriendofzerg@gmail.com>
2018-08-26 19:17:50 +1000

    Testing

---
+++ src/test/java/pmdRules/DuplicatesTest.java
@@ -0,0 +1,30 @@
+package pmdRules;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class DuplicatesTest extends BaseXmlPmdRuleTest {
+
+    private static String testFile = "DuplicatesTest.pas";
+    private static String ruleType = "DuplicatesRule"; // Rule type being tested
+
+    @Test
+    public void testRuleViolation() {
+
+        super.analyse(); // fixme, be better if didnt have to run this every test, just once
+        ArrayList<ArrayList<Object>> fileResults = getFileResults(testFile);
+
+        Integer[] expectedViolationLines = {25, 29, 34};
+
+        // Collect the relevant results
+        ArrayList violationLines = super.getViolationLines(fileResults, ruleType);
+
+        assertEquals(violationLines, Arrays.asList(expectedViolationLines));
+    }
+
+
+}
---
+++ src/test/java/pmdRules/TooManySubProceduresTest.java
@@ -0,0 +1,29 @@
+package pmdRules;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class TooManySubProceduresTest extends BaseXmlPmdRuleTest {
+
+    private static String testFile = "TooManySubProceduresTest.pas";
+    private static String ruleType = "TooManySubProceduresRule"; // Rule type being tested
+
+    @Test
+    public void testRuleViolation() {
+
+        super.analyse(); // fixme, be better if didnt have to run this every test, just once
+        ArrayList<ArrayList<Object>> fileResults = getFileResults(testFile);
+
+        Integer[] expectedViolationLines = {19};
+
+        // Collect the relevant results
+        ArrayList violationLines = super.getViolationLines(fileResults, ruleType);
+
+        assertEquals(violationLines, Arrays.asList(expectedViolationLines));
+    }
+
+}
