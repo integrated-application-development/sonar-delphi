2dfaad1e2ec58ac09499c7a187f8e2d74d468ce4
alecfn <apfor2@student.monash.edu>
2018-09-13 16:04:42 +1000

    Test naming conventions in core and colorizer packages.

--- src/test/java/org/sonar/plugins/delphi/core/language/impl/DelphiClassTest.java
+++ src/test/java/org/sonar/plugins/delphi/core/language/impl/DelphiClassTest.java
@@ -32,9 +32,9 @@ import org.sonar.plugins.delphi.core.language.FunctionInterface;
 
 public class DelphiClassTest {
 
-  DelphiClass ancestor = null;
-  DelphiClass parent = null;
-  DelphiClass child = null;
+  private DelphiClass ancestor = null;
+  private DelphiClass parent = null;
+  private DelphiClass child = null;
 
   @Before
   public void init() {
@@ -46,20 +46,20 @@ public class DelphiClassTest {
   }
 
   @Test
-  public void getFileNameTest() {
+  public void testGetFileName() {
     parent.setFileName("MyFile.pas");
     assertEquals("MyFile.pas", parent.getFileName());
   }
 
   @Test
-  public void getVisibilityTest() {
+  public void testGetVisibility() {
     assertEquals(DelphiLexer.PRIVATE, parent.getVisibility());
     parent.setVisibility(DelphiLexer.PUBLIC);
     assertEquals(DelphiLexer.PUBLIC, parent.getVisibility());
   }
 
   @Test
-  public void getPublicApiCountTest() {
+  public void testGetPublicApiCount() {
     FunctionInterface func = new DelphiFunction("public");
     ClassFieldInterface field = new DelphiClassField();
     field.setVisibility(DelphiLexer.PUBLIC);
@@ -74,13 +74,13 @@ public class DelphiClassTest {
   }
 
   @Test
-  public void getNameTest() {
+  public void testGetName() {
     assertEquals("parent", parent.getName());
     assertEquals("parent", parent.getShortName());
   }
 
   @Test
-  public void getComplexityTest() {
+  public void testComplexity() {
     FunctionInterface func = new DelphiFunction("parent.foo");
     FunctionInterface func2 = new DelphiFunction("parent.setBar");
     func.setComplexity(2);
@@ -93,7 +93,7 @@ public class DelphiClassTest {
   }
 
   @Test
-  public void hasFunctionTest() {
+  public void testHasFunction() {
     FunctionInterface foo = new DelphiFunction("foo");
     FunctionInterface bar = new DelphiFunction("bar");
 
@@ -115,7 +115,7 @@ public class DelphiClassTest {
   }
 
   @Test
-  public void getAccessorsCountTest() {
+  public void testGetAccessorsCount() {
     ancestor.addFunction(new DelphiFunction("blah"));
     assertEquals(0, ancestor.getAccessorCount());
     ancestor.addFunction(new DelphiFunction("ancestor.setfield"));
@@ -127,7 +127,7 @@ public class DelphiClassTest {
   }
 
   @Test
-  public void getChildrenTest() {
+  public void testGetChildren() {
     assertEquals(1, ancestor.getChildren().length);
     assertEquals(1, parent.getChildren().length);
     assertEquals(0, child.getChildren().length);
@@ -138,7 +138,7 @@ public class DelphiClassTest {
   }
 
   @Test
-  public void getDescendantsTest() {
+  public void testGetDescendants() {
     parent.addChild(new DelphiClass(null));
     child.addParent(new DelphiClass(null));
     assertEquals(3, ancestor.getDescendants().length);
