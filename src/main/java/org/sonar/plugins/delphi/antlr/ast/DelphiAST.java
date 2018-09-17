0d63104755d638820a48c30d038a34a67fd2cb14
Alec <sdfasdf>
2018-09-17 11:22:47 +1000

    Just a minor change to String format syntax.

--- src/main/java/org/sonar/plugins/delphi/antlr/ast/DelphiAST.java
+++ src/main/java/org/sonar/plugins/delphi/antlr/ast/DelphiAST.java
@@ -168,7 +168,7 @@ public class DelphiAST extends CommonTree implements ASTTree {
     try {
       return generateDocument(createNewDocument(), "file");
     } catch (Exception e) {
-      DelphiUtils.LOG.error(String.format("%s %s %s", toString() , "Could not generate xml document: " , e.getMessage()));
+      DelphiUtils.LOG.error("{} {}" , "Could not generate xml document: " , e.getMessage());
       return null;
     }
   }
@@ -229,7 +229,7 @@ public class DelphiAST extends CommonTree implements ASTTree {
       NodeName nodeName = NodeName.findByCode(code);
       return nodeName.getName();
     } catch (NodeNameForCodeDoesNotExistException e) {
-      DelphiUtils.LOG.error(String.format("%s %s %s", toString(), "NODE name for code does not exist", e.getMessage()));
+      DelphiUtils.LOG.error("{} {} {}", toString(), "NODE name for code does not exist", e.getMessage());
     }
     return code;
   }
