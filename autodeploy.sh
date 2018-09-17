ec0f0370700ca43e808ab37ad13180b39fe51d8d
Mayank Adlakha <mayankadlakha@gmail.com>
2018-09-17 11:44:41 +1000

    Fixed all code smells for DelphiSureFireParser.java

--- autodeploy.sh
+++ autodeploy.sh
@@ -6,7 +6,12 @@ echo "building project"
 mvn clean install
 
 echo "moving compiled .jar file to sonarqube extensions"
-cp target/sonar-delphi-plugin-0.1.jar <YOUR PATH TO SONARQUBE>/sonarqube-7.2.1/extensions/plugins
+cp target/sonar-delphi-plugin-0.1.jar ~/Documents/Monash/FIT4002/sonarqube-7.2/extensions/plugins
 
 echo "starting up sonarqube"
-<YOUR PATH TO SONARQUBE>/sonarqube-7.2.1/bin/<YOUR OS>/sonar.sh console
+~/Documents/Monash/FIT4002/sonarqube-7.2/bin/macosx-universal-64/sonar.sh console
+
+#sleep 30
+
+#cd ~/Documents/Monash/FIT4002/TheAgileExcuse/FIT4002_SonarQube_Delphi/src/main/delphi/
+#~/Documents/Monash/FIT4002/sonar-scanner-3.2.0.1227-macosx/bin/./sonar-scanner
