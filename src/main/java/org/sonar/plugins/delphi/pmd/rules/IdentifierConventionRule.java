68f58583237bcc2c9df52bc71971f9d18be87f6b
Julian Farinaccio <jkfar4@student.monash.edu>
2018-10-22 12:28:13 +1100

    Added IdentifierConvention Rule

---
+++ src/main/java/org/sonar/plugins/delphi/pmd/rules/IdentifierConventionRule.java
@@ -0,0 +1,18 @@
+package org.sonar.plugins.delphi.pmd.rules;
+
+import net.sourceforge.pmd.RuleContext;
+import org.sonar.plugins.delphi.antlr.DelphiLexer;
+import org.sonar.plugins.delphi.antlr.ast.DelphiPMDNode;
+
+public class IdentifierConventionRule extends DelphiRule {
+    public void visit(DelphiPMDNode node, RuleContext ctx) {
+        if (node.getType() == DelphiLexer.TkIdentifier) {
+            char firstChar = node.getText().charAt(0);
+            if (firstChar != Character.toUpperCase(firstChar)) {
+                addViolation(ctx, node);
+            }
+
+        }
+    }
+}
+
