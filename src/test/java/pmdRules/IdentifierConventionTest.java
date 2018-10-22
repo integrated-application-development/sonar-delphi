dc8a1609167e6b818570c56b85b3dec31315963c
Julian Farinaccio <jkfar4@student.monash.edu>
2018-10-22 12:39:09 +1100

    added JUnit test for IdentifierConventionRule

---
+++ src/test/java/pmdRules/IdentifierConventionTest.java
@@ -0,0 +1,39 @@
+package pmdRules;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class IdentifierConventionTest extends BaseXmlPmdRuleTest {
+    private static String ruleType = "IdentifierConventionRule"; // Rule type being tested
+
+    @Test
+    public void testRuleViolation() {
+
+        String testFile = "IdentifierConventionTest.pas";
+        super.testAnalyse();
+        ArrayList<ArrayList<Object>> fileResults = getFileResults(testFile);
+
+        Integer[] expectedViolationLines = {9};
+
+        // Collect the relevant results
+        ArrayList violationLines = super.getViolationLines(fileResults, ruleType);
+
+        assertEquals(violationLines, Arrays.asList(expectedViolationLines));
+    }
+
+    @Test
+    public void testNoViolation(){
+        String testFile = "InterfaceNameTest.pas";
+        super.testAnalyse();
+        ArrayList<ArrayList<Object>> fileResults = getFileResults(testFile);
+
+        ArrayList violationLines = super.getViolationLines(fileResults, ruleType);
+
+        assertEquals(0, violationLines.size());
+    }
+
+}
