83370dee25bde162717ca2b613e7aece997b1058
Julian Farinaccio <jkfar4@student.monash.edu>
2018-10-22 12:57:26 +1100

    Updates to Identifier convention tests.

--- src/main/java/org/sonar/plugins/delphi/pmd/rules/IdentifierConventionRule.java
+++ src/main/java/org/sonar/plugins/delphi/pmd/rules/IdentifierConventionRule.java
@@ -5,6 +5,12 @@ import org.sonar.plugins.delphi.antlr.DelphiLexer;
 import org.sonar.plugins.delphi.antlr.ast.DelphiPMDNode;
 
 public class IdentifierConventionRule extends DelphiRule {
+    /**
+     * This rule looks at all identifiers at ensures it follows the Delphi Case convention
+     *
+     *  @param node the current node
+     *  @param ctx the ruleContext to store the violations
+     */
     public void visit(DelphiPMDNode node, RuleContext ctx) {
         if (node.getType() == DelphiLexer.TkIdentifier) {
             char firstChar = node.getText().charAt(0);
