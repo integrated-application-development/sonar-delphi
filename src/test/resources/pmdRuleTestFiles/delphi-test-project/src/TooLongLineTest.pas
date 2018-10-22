0450bd712afa2549ba52c3008643db5b881f9e3d
Mayank Adlakha <mayankadlakha@gmail.com>
2018-10-22 14:12:06 +1100

    Tests added for TooLongLineRule

---
+++ src/test/resources/pmdRuleTestFiles/delphi-test-project/src/TooLongLineTest.pas
@@ -0,0 +1,47 @@
+unit TooLongLineTest;
+
+interface
+
+uses
+  Windows;
+
+type
+  TStatementTest = class(TForm)
+  public
+    procedure fooZZ;
+  private
+    procedure bar;
+  end;
+
+implementation
+
+const //Non-compliant: line below longer than 100 characters
+  HELLO = 'Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World';
+
+      //Non-compliant: comment below longer than 100 characters
+//This is a comment This is a comment This is a comment This is a comment This is a comment This is a comment This is a comment
+
+procedure TStatementTest.fooZZ;
+var
+  x,y: integer;
+begin
+  x := 0;
+  y := 1;
+  x := y + 5;
+  y := x - 6;
+end;
+
+procedure TStatementTest.bar;
+begin
+  if(5 > 4) then
+  begin
+    writeln('5 > 4');
+  end
+  else
+  begin
+    writeln('5 <= 4');
+  end;
+
+end;
+
+end.
