914f5d9ea6425b9180e80599f064c959164e71ad
Jonah Jeleniewski <jjeleniewski@integradev.com.au>
2019-07-12 13:00:25 +1000

    Add more pmd rule tests

---
+++ src/test/java/org/sonar/plugins/delphi/pmd/rules/NoGuidRuleTest.java
@@ -0,0 +1,44 @@
+package org.sonar.plugins.delphi.pmd.rules;
+
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.sonar.plugins.delphi.IssueMatchers.hasRuleKeyAtLine;
+
+import org.junit.Test;
+import org.sonar.plugins.delphi.pmd.DelphiTestUnitBuilder;
+
+public class NoGuidRuleTest extends BasePmdRuleTest {
+  @Test
+  public void testInterfaceWithGuidShouldNotAddIssue() {
+    DelphiTestUnitBuilder builder = new DelphiTestUnitBuilder()
+        .appendDecl("type")
+        .appendDecl("  IPublisher = interface")
+        .appendDecl("    ['{E1787C21-0FF2-11D5-A978-006067000685}']")
+        .appendDecl("      procedure RegisterSubscriber(Handler: TNotifyEvent);")
+        .appendDecl("      procedure DeregisterSubscriber(Handler: TNotifyEvent);")
+        .appendDecl("      procedure Notify(Event: TObject);")
+        .appendDecl("end;");
+
+    execute(builder);
+
+    assertIssues(empty());
+  }
+
+  @Test
+  public void testInterfaceWithoutGuidShouldAddIssue() {
+    DelphiTestUnitBuilder builder = new DelphiTestUnitBuilder()
+        .appendDecl("type")
+        .appendDecl("  IPublisher = interface")
+        .appendDecl("    procedure RegisterSubscriber(Handler: TNotifyEvent);")
+        .appendDecl("    procedure DeregisterSubscriber(Handler: TNotifyEvent);")
+        .appendDecl("    procedure Notify(Event: TObject);")
+        .appendDecl("  end;");
+
+    execute(builder);
+
+    assertIssues(hasSize(1));
+    assertIssues(hasItem(hasRuleKeyAtLine("NoGuidRule", builder.getOffsetDecl() + 2)));
+  }
+
+}
