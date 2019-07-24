09609818d4dfce753da66214eccf316b7de95c63
Jonah Jeleniewski <jjeleniewski@integradev.com.au>
2019-07-24 17:47:26 +1000

    Fix VariableNameRule false-positives on AutoCreateForm generated variables

--- src/main/java/org/sonar/plugins/delphi/pmd/rules/VariableNameRule.java
+++ src/main/java/org/sonar/plugins/delphi/pmd/rules/VariableNameRule.java
@@ -1,5 +1,6 @@
 package org.sonar.plugins.delphi.pmd.rules;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,17 +17,23 @@ public class VariableNameRule extends NameConventionRule {
   @Override
   public List<DelphiPMDNode> findNodes(DelphiPMDNode node) {
     // Within a var block, look for variable identifiers
-    if (node.getType() == DelphiLexer.VAR && node.getChildren() != null) {
+    if (shouldVisit(node)) {
+      List<DelphiPMDNode> nameNodes = new ArrayList<>();
+
       for (Object child : node.getChildren()) {
         CommonTree childNode = (CommonTree) child;
 
         if (childNode.getType() == DelphiLexer.TkVariableIdents) {
           List<?> children = childNode.getChildren();
 
-          return children.stream()
+          nameNodes.addAll(children.stream()
               .map(varName -> new DelphiPMDNode((CommonTree) varName, node.getASTTree()))
-              .collect(Collectors.toList());
+              .collect(Collectors.toList()));
         }
+      }
+
+      if (isNotAutoCreateFormVar(node, nameNodes)) {
+        return Collections.unmodifiableList(nameNodes);
       }
     }
 
@@ -37,6 +44,15 @@ public class VariableNameRule extends NameConventionRule {
   protected boolean isViolation(DelphiPMDNode nameNode) {
     String name = nameNode.getText();
     return !Character.isUpperCase(name.charAt(0));
+  }
+
+  private boolean shouldVisit(DelphiPMDNode node) {
+    return node.getType() == DelphiLexer.VAR && node.getChildCount() != 0;
+  }
+
+  private boolean isNotAutoCreateFormVar(DelphiPMDNode node, List<DelphiPMDNode> nameNodes) {
+    return !(isInterfaceSection() && nameNodes.size() == 1
+        && node.getChildIndex() == node.getParent().getChildCount() - 1);
   }
 }
 
---
+++ src/test/java/org/sonar/plugins/delphi/pmd/rules/VariableNameRuleTest.java
@@ -0,0 +1,96 @@
+package org.sonar.plugins.delphi.pmd.rules;
+
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.not;
+import static org.sonar.plugins.delphi.IssueMatchers.hasRuleKeyAtLine;
+
+import org.junit.Test;
+import org.sonar.plugins.delphi.pmd.DelphiTestUnitBuilder;
+
+public class VariableNameRuleTest extends BasePmdRuleTest {
+  @Test
+  public void testValidNamesShouldNotAddIssue() {
+    DelphiTestUnitBuilder builder = new DelphiTestUnitBuilder();
+
+    builder.appendDecl("var");
+    builder.appendDecl("  MyChar: Char;");
+    builder.appendDecl("  AnotherChar: Char;");
+    builder.appendDecl("  ThirdChar: Char;");
+
+    execute(builder);
+
+    assertIssues(empty());
+  }
+
+  @Test
+  public void testValidNameInMethodShouldNotAddIssue() {
+    DelphiTestUnitBuilder builder = new DelphiTestUnitBuilder();
+    builder.appendImpl("procedure MyProcedure;");
+    builder.appendImpl("var");
+    builder.appendImpl("  SomeVar: Integer;");
+    builder.appendImpl("begin");
+    builder.appendImpl("  if SomeVar <> 0 then begin");
+    builder.appendImpl("    WriteLn('test');");
+    builder.appendImpl("  end;");
+    builder.appendImpl("end;");
+
+    execute(builder);
+
+    assertIssues(empty());
+  }
+
+  @Test
+  public void testBadPascalCaseShouldAddIssue() {
+    DelphiTestUnitBuilder builder = new DelphiTestUnitBuilder();
+
+    builder.appendDecl("var");
+    builder.appendDecl("  MyChar: Char;");
+    builder.appendDecl("  AnotherChar: Char;");
+    builder.appendDecl("  thirdChar: Char;");
+
+    execute(builder);
+
+    assertIssues(hasSize(1));
+    assertIssues(hasItem(hasRuleKeyAtLine("VariableNameRule", builder.getOffsetDecl() + 4)));
+  }
+
+  @Test
+  public void testBadPascalCaseInMethodShouldAddIssue() {
+    DelphiTestUnitBuilder builder = new DelphiTestUnitBuilder();
+    builder.appendImpl("procedure MyProcedure;");
+    builder.appendImpl("var");
+    builder.appendImpl("  someVar: Integer;");
+    builder.appendImpl("begin");
+    builder.appendImpl("  if someVar <> 0 then begin");
+    builder.appendImpl("    WriteLn('test');");
+    builder.appendImpl("  end;");
+    builder.appendImpl("end;");
+
+    execute(builder);
+
+    assertIssues(hasSize(1));
+    assertIssues(hasItem(hasRuleKeyAtLine("VariableNameRule", builder.getOffSet() + 3)));
+  }
+
+  @Test
+  public void testAutoCreateFormVariableShouldNotAddIssue() {
+    DelphiTestUnitBuilder builder = new DelphiTestUnitBuilder();
+
+    builder.appendDecl("var");
+    builder.appendDecl("  MyChar: Char;");
+    builder.appendDecl("  AnotherChar: Char;");
+    builder.appendDecl("  thirdChar: Char;");
+
+    builder.appendDecl("var");
+    builder.appendDecl("  omForm: TForm;");
+
+    execute(builder);
+
+    assertIssues(hasSize(1));
+    assertIssues(hasItem(hasRuleKeyAtLine("VariableNameRule", builder.getOffsetDecl() + 4)));
+    assertIssues(not(hasItem(hasRuleKeyAtLine("VariableNameRule", builder.getOffsetDecl() + 6))));
+  }
+
+}
