c5f2e0fad028aa0c1d1f31e35608e3aa471b4f1f
Jonah Jeleniewski <jjeleniewski@integradev.com.au>
2019-11-20 17:19:00 +1100

    Exclude class constructors in `ConstructorCreateRule`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 - Enhance `MixedNamesRule` with symbol table information.
+- Exclude class constructors in `ConstructorCreateRule`.
 
 ### Fixed
 
--- src/main/java/org/sonar/plugins/delphi/pmd/rules/ConstructorCreateRule.java
+++ src/main/java/org/sonar/plugins/delphi/pmd/rules/ConstructorCreateRule.java
@@ -1,22 +1,22 @@
 package org.sonar.plugins.delphi.pmd.rules;
 
 import net.sourceforge.pmd.RuleContext;
-import org.sonar.plugins.delphi.antlr.ast.node.MethodHeadingNode;
+import org.sonar.plugins.delphi.antlr.ast.node.MethodDeclarationNode;
 import org.sonar.plugins.delphi.utils.NameConventionUtils;
 
 public class ConstructorCreateRule extends AbstractDelphiRule {
   private static final String PREFIX = "Create";
 
   @Override
-  public RuleContext visit(MethodHeadingNode method, RuleContext data) {
+  public RuleContext visit(MethodDeclarationNode method, RuleContext data) {
     if (isViolation(method)) {
-      addViolation(data, method);
+      addViolation(data, method.getMethodName());
     }
     return super.visit(method, data);
   }
 
-  private boolean isViolation(MethodHeadingNode method) {
-    if (method.isMethodImplementation() || !method.isConstructor()) {
+  private boolean isViolation(MethodDeclarationNode method) {
+    if (!method.isConstructor() || method.isClassMethod()) {
       return false;
     }
 
