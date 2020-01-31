48f03dc0e41f3bd533b61ef4dec249d7bee229ee
Jonah Jeleniewski <jjeleniewski@integradev.com.au>
2020-01-31 15:36:02 +1100

    Architecture tests

--- src/test/java/org/sonar/plugins/delphi/antlr/ast/DelphiNodeTest.java
+++ src/test/java/org/sonar/plugins/delphi/antlr/ast/DelphiNodeTest.java
@@ -1,58 +1,62 @@
 package org.sonar.plugins.delphi.antlr.ast;
 
-import static java.util.function.Predicate.not;
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
+import static org.sonar.plugins.delphi.antlr.ast.DelphiNodeUtils.ARE_DELPHI_NODES;
+import static org.sonar.plugins.delphi.antlr.ast.DelphiNodeUtils.HAVE_TOKEN_CONSTRUCTOR;
+import static org.sonar.plugins.delphi.antlr.ast.DelphiNodeUtils.IMPLEMENT_ACCEPT;
+import static org.sonar.plugins.delphi.antlr.ast.DelphiNodeUtils.NODE_PACKAGE;
 
-import java.lang.reflect.Modifier;
-import java.util.Set;
-import java.util.stream.Collectors;
+import com.tngtech.archunit.core.domain.JavaModifier;
 import org.junit.Test;
 import org.sonar.plugins.delphi.antlr.ast.node.DelphiNode;
 import org.sonar.plugins.delphi.antlr.ast.visitors.DelphiParserVisitor;
 
 public class DelphiNodeTest {
-  private static final String MISSING_MODIFIERS = "Expected abstract or final modifier in %s";
-  private static final String ABSTRACT_ACCEPT_IMPLEMENTED =
-      "Expected accept not to be implemented in abstract nodes %s";
-
   @Test
   public void testAllNodesShouldBeAbstractOrFinal() {
-    Set<String> failed =
-        DelphiNodeUtils.getNodeTypes().stream()
-            .filter(not(DelphiNodeUtils::hasExpectedModifiers))
-            .map(Class::getSimpleName)
-            .collect(Collectors.toSet());
+    classes()
+        .that(ARE_DELPHI_NODES)
+        .should()
+        .haveModifier(JavaModifier.ABSTRACT)
+        .orShould()
+        .haveModifier(JavaModifier.FINAL)
+        .check(NODE_PACKAGE);
+  }
 
-    assertThat(failed).as(String.format(MISSING_MODIFIERS, failed)).isEmpty();
+  @Test
+  public void testAllNodesShouldEndNameWithNode() {
+    classes().that(ARE_DELPHI_NODES).should().haveSimpleNameEndingWith("Node").check(NODE_PACKAGE);
   }
 
   @Test
   public void testAbstractNodesShouldNotImplementAccept() {
-    Set<String> failed =
-        DelphiNodeUtils.getNodeTypes().stream()
-            .filter(type -> Modifier.isAbstract(type.getModifiers()))
-            .filter(DelphiNodeUtils::implementsAccept)
-            .map(Class::getSimpleName)
-            .collect(Collectors.toSet());
-
-    assertThat(failed).as(String.format(ABSTRACT_ACCEPT_IMPLEMENTED, failed)).isEmpty();
+    classes()
+        .that(ARE_DELPHI_NODES)
+        .and(IMPLEMENT_ACCEPT)
+        .should()
+        .notHaveModifier(JavaModifier.ABSTRACT)
+        .check(NODE_PACKAGE);
   }
 
   @Test
   public void testAcceptShouldCallCorrectVisitorMethod() {
     for (DelphiNode node : DelphiNodeUtils.getNodeInstances()) {
-      DelphiParserVisitor<?> visitor = spy(new DelphiParserVisitor() {});
+      DelphiParserVisitor<?> visitor = spy(new DelphiParserVisitor<>() {});
       node.accept(visitor, null);
       verify(visitor, atLeastOnce()).visit(any(node.getClass()), any());
     }
   }
 
   @Test
-  public void testAllNodesCanBeInstantiated() {
-    DelphiNodeUtils.getNodeInstances();
+  public void testAllNodesHaveTokenConstructor() {
+    classes()
+        .that(ARE_DELPHI_NODES)
+        .should(HAVE_TOKEN_CONSTRUCTOR)
+        .because("this is needed for reflective instantiation")
+        .check(NODE_PACKAGE);
   }
 }
