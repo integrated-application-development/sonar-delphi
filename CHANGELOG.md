39d30296e5d2ce4c4763777e76b6406ff4e5fd98
Jonah Jeleniewski <jjeleniewski@integradev.com.au>
2021-03-23 10:44:00 +1100

    Implement `PlatformDependentCastRule`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - `NoSonarRule` analysis rule, which flags usages of the `NOSONAR` marker.
+- `PlatformDependentCastRule` analysis rule, which flags casts between platform-dependent and
+  platform-independent types.
 
 ## [0.28.0] - 2021-02-02
 
---
+++ src/main/java/org/sonar/plugins/delphi/pmd/rules/PlatformDependentCastRule.java
@@ -0,0 +1,71 @@
+package org.sonar.plugins.delphi.pmd.rules;
+
+import java.util.List;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.lang.ast.Node;
+import org.sonar.plugins.delphi.antlr.ast.node.ArgumentListNode;
+import org.sonar.plugins.delphi.antlr.ast.node.ExpressionNode;
+import org.sonar.plugins.delphi.antlr.ast.node.NameReferenceNode;
+import org.sonar.plugins.delphi.symbol.declaration.DelphiNameDeclaration;
+import org.sonar.plugins.delphi.symbol.declaration.TypeNameDeclaration;
+import org.sonar.plugins.delphi.type.DelphiType;
+import org.sonar.plugins.delphi.type.Type;
+import org.sonar.plugins.delphi.type.Type.ProceduralType;
+import org.sonar.plugins.delphi.type.TypeUtils;
+import org.sonar.plugins.delphi.type.intrinsic.IntrinsicType;
+
+public class PlatformDependentCastRule extends AbstractDelphiRule {
+  @Override
+  public RuleContext visit(ArgumentListNode argumentList, RuleContext data) {
+    List<ExpressionNode> arguments = argumentList.getArguments();
+    if (arguments.size() == 1) {
+      ExpressionNode expression = arguments.get(0);
+      if (!expression.isLiteral()) {
+        Type originalType = getOriginalType(expression);
+        Type castedType = getHardCastedType(argumentList);
+
+        if (isPlatformDependentCast(originalType, castedType)) {
+          addViolation(data, argumentList);
+        }
+      }
+    }
+    return super.visit(argumentList, data);
+  }
+
+  private static Type getOriginalType(ExpressionNode expression) {
+    Type result = expression.getType();
+    if (result.isMethod()) {
+      result = ((ProceduralType) result).returnType();
+    }
+    return result;
+  }
+
+  private static Type getHardCastedType(ArgumentListNode argumentList) {
+    Node previous = argumentList.jjtGetParent().jjtGetChild(argumentList.jjtGetChildIndex() - 1);
+    if (previous instanceof NameReferenceNode) {
+      NameReferenceNode nameReference = ((NameReferenceNode) previous);
+      DelphiNameDeclaration declaration = nameReference.getLastName().getNameDeclaration();
+      if (declaration instanceof TypeNameDeclaration) {
+        return ((TypeNameDeclaration) declaration).getType();
+      }
+    }
+    return DelphiType.unknownType();
+  }
+
+  private static boolean isPlatformDependentCast(Type originalType, Type castedType) {
+    return isApplicableType(originalType)
+        && isApplicableType(castedType)
+        && isPlatformDependentType(originalType) != isPlatformDependentType(castedType);
+  }
+
+  private static boolean isApplicableType(Type type) {
+    return type.isPointer() || type.isInteger();
+  }
+
+  private static boolean isPlatformDependentType(Type type) {
+    type = TypeUtils.findBaseType(type);
+    return type.isPointer()
+        || type.is(IntrinsicType.NATIVEINT)
+        || type.is(IntrinsicType.NATIVEUINT);
+  }
+}
