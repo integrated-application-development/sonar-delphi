38bbdcfa768d95d6210be47dde104f40644f340e
Jonah Jeleniewski <jjeleniewski@integradev.com.au>
2022-02-24 14:18:30 +1100

    Upgrade integration tests to JUnit5

--- src/it/java/org/sonar/plugins/delphi/DelphiCpdExecutorIT.java
+++ src/it/java/org/sonar/plugins/delphi/DelphiCpdExecutorIT.java
@@ -4,34 +4,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.sonar.plugins.delphi.IntegrationTestSuite.createScanner;
 import static org.sonar.plugins.delphi.IntegrationTestSuite.getProjectMeasureAsDouble;
 
-import com.sonar.orchestrator.Orchestrator;
-import org.junit.ClassRule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.sonar.plugins.delphi.extension.OrchestratorExtension;
 
-public class DelphiCpdExecutorIT {
+class DelphiCpdExecutorIT {
   private static final String SIMPLE_PROJECT_KEY = "delphi-cpd-simple-test";
   private static final String LITERALS_PROJECT_KEY = "delphi-cpd-literals-test";
   private static final String WHITESPACE_PROJECT_KEY = "delphi-cpd-whitespace-test";
 
-  @ClassRule public static final Orchestrator ORCHESTRATOR = IntegrationTestSuite.ORCHESTRATOR;
+  @RegisterExtension
+  static final OrchestratorExtension ORCHESTRATOR = IntegrationTestSuite.ORCHESTRATOR;
 
   @Test
-  public void testShouldDetectDuplicates() {
-    ORCHESTRATOR.executeBuild(createScanner("cpd-simple", SIMPLE_PROJECT_KEY));
+  void testShouldDetectDuplicates() {
+    ORCHESTRATOR.getOrchestrator().executeBuild(createScanner("cpd-simple", SIMPLE_PROJECT_KEY));
     assertThat(getProjectMeasureAsDouble("duplicated_lines", SIMPLE_PROJECT_KEY)).isEqualTo(26.0);
     assertThat(getProjectMeasureAsDouble("duplicated_blocks", SIMPLE_PROJECT_KEY)).isEqualTo(2.0);
   }
 
   @Test
-  public void testShouldDetectDuplicatesByNormalizingLiterals() {
-    ORCHESTRATOR.executeBuild(createScanner("cpd-literals", LITERALS_PROJECT_KEY));
+  void testShouldDetectDuplicatesByNormalizingLiterals() {
+    ORCHESTRATOR
+        .getOrchestrator()
+        .executeBuild(createScanner("cpd-literals", LITERALS_PROJECT_KEY));
     assertThat(getProjectMeasureAsDouble("duplicated_lines", LITERALS_PROJECT_KEY)).isEqualTo(26.0);
     assertThat(getProjectMeasureAsDouble("duplicated_blocks", LITERALS_PROJECT_KEY)).isEqualTo(2.0);
   }
 
   @Test
-  public void testShouldDetectDuplicatesByIgnoringWhitespace() {
-    ORCHESTRATOR.executeBuild(createScanner("cpd-whitespace", WHITESPACE_PROJECT_KEY));
+  void testShouldDetectDuplicatesByIgnoringWhitespace() {
+    ORCHESTRATOR
+        .getOrchestrator()
+        .executeBuild(createScanner("cpd-whitespace", WHITESPACE_PROJECT_KEY));
     assertThat(getProjectMeasureAsDouble("duplicated_lines", WHITESPACE_PROJECT_KEY))
         .isEqualTo(33.0);
     assertThat(getProjectMeasureAsDouble("duplicated_blocks", WHITESPACE_PROJECT_KEY))
--- src/it/java/org/sonar/plugins/delphi/IntegrationTestSuite.java
+++ src/it/java/org/sonar/plugins/delphi/IntegrationTestSuite.java
@@ -2,16 +2,14 @@ package org.sonar.plugins.delphi;
 
 import static java.util.Collections.singletonList;
 
-import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.build.SonarScanner;
 import com.sonar.orchestrator.locator.FileLocation;
 import java.io.File;
 import java.util.List;
-import org.junit.ClassRule;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.platform.suite.api.SelectClasses;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.Suite;
+import org.sonar.plugins.delphi.extension.OrchestratorExtension;
 import org.sonar.plugins.delphi.utils.DelphiUtils;
 import org.sonarqube.ws.Measures.ComponentWsResponse;
 import org.sonarqube.ws.Measures.Measure;
@@ -20,36 +18,25 @@ import org.sonarqube.ws.client.WsClient;
 import org.sonarqube.ws.client.WsClientFactories;
 import org.sonarqube.ws.client.measures.ComponentRequest;
 
-@SuppressWarnings("JUnit5Platform")
-@RunWith(JUnitPlatform.class)
-@IncludeClassNamePatterns("^.*IT$")
+@Suite
 @SelectClasses({DelphiCpdExecutorIT.class})
-public class IntegrationTestSuite {
+class IntegrationTestSuite {
   private static final String PROJECTS_PATH = "src/it/projects/";
   private static final String STANDARD_LIBRARY_PATH =
       DelphiUtils.getResource("/org/sonar/plugins/delphi/standardLibrary").getAbsolutePath();
 
-  private static final FileLocation PLUGIN_LOCATION =
-      FileLocation.byWildcardMavenFilename(new File("target"), "sonar-delphi-plugin-*.jar");
-
-  @ClassRule
-  public static final Orchestrator ORCHESTRATOR =
-      Orchestrator.builderEnv()
-          .setSonarVersion(System.getProperty("sonar.runtimeVersion", "LATEST_RELEASE"))
-          .addPlugin(PLUGIN_LOCATION)
-          .build();
+  @RegisterExtension static final OrchestratorExtension ORCHESTRATOR = new OrchestratorExtension();
 
   private IntegrationTestSuite() {
     // Hide public constructor
   }
 
-  public static Orchestrator getOrchestrator() {
-    return ORCHESTRATOR;
-  }
-
   private static WsClient newWsClient() {
     return WsClientFactories.getDefault()
-        .newClient(HttpConnector.newBuilder().url(ORCHESTRATOR.getServer().getUrl()).build());
+        .newClient(
+            HttpConnector.newBuilder()
+                .url(ORCHESTRATOR.getOrchestrator().getServer().getUrl())
+                .build());
   }
 
   public static Double getProjectMeasureAsDouble(String metricKey, String projectKey) {
