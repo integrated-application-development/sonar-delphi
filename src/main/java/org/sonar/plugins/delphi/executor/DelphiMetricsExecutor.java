169c57da1acc7580f79c9be08f557fe60af5892d
Elliot Hillary <ehillary@integradev.com.au>
2022-11-30 13:28:26 +1100

    Add NCLOC_DATA metric to record which lines contain code

--- src/main/java/org/sonar/plugins/delphi/executor/DelphiMetricsExecutor.java
+++ src/main/java/org/sonar/plugins/delphi/executor/DelphiMetricsExecutor.java
@@ -19,8 +19,11 @@
 package org.sonar.plugins.delphi.executor;
 
 import java.io.Serializable;
+import java.util.Set;
 import org.sonar.api.batch.sensor.SensorContext;
 import org.sonar.api.measures.CoreMetrics;
+import org.sonar.api.measures.FileLinesContext;
+import org.sonar.api.measures.FileLinesContextFactory;
 import org.sonar.api.measures.Metric;
 import org.sonar.plugins.delphi.antlr.ast.visitors.MetricsVisitor;
 import org.sonar.plugins.delphi.antlr.ast.visitors.MetricsVisitor.Data;
@@ -29,7 +32,12 @@ import org.sonar.plugins.delphi.file.DelphiFile.DelphiInputFile;
 public class DelphiMetricsExecutor implements Executor {
   private static final MetricsVisitor VISITOR = new MetricsVisitor();
   private SensorContext context;
+  private final FileLinesContextFactory fileLinesContextFactory;
   private DelphiInputFile file;
+
+  public DelphiMetricsExecutor(FileLinesContextFactory fileLinesContextFactory) {
+    this.fileLinesContextFactory = fileLinesContextFactory;
+  }
 
   @Override
   public void execute(Context context, DelphiInputFile file) {
@@ -43,11 +51,23 @@ public class DelphiMetricsExecutor implements Executor {
     saveMetricOnFile(CoreMetrics.COMPLEXITY, metrics.getComplexity());
     saveMetricOnFile(CoreMetrics.COMMENT_LINES, metrics.getCommentLines());
     saveMetricOnFile(CoreMetrics.STATEMENTS, metrics.getStatements());
-    saveMetricOnFile(CoreMetrics.NCLOC, metrics.getCodeLines());
     saveMetricOnFile(CoreMetrics.COGNITIVE_COMPLEXITY, metrics.getCognitiveComplexity());
+
+    Set<Integer> codeLines = metrics.getCodeLines();
+    saveMetricOnFile(CoreMetrics.NCLOC, codeLines.size());
+    saveCodeLinesOnFile(codeLines);
   }
 
   private <T extends Serializable> void saveMetricOnFile(Metric<T> metric, T value) {
     context.<T>newMeasure().forMetric(metric).on(file.getInputFile()).withValue(value).save();
+  }
+
+  private void saveCodeLinesOnFile(Set<Integer> codeLines) {
+    FileLinesContext fileLinesContext = fileLinesContextFactory.createFor(file.getInputFile());
+    for (int line = 1; line <= file.getInputFile().lines(); line++) {
+      fileLinesContext.setIntValue(
+          CoreMetrics.NCLOC_DATA_KEY, line, codeLines.contains(line) ? 1 : 0);
+    }
+    fileLinesContext.save();
   }
 }
