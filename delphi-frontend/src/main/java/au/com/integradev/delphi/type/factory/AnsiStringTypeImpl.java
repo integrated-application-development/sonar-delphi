23b17d875821d323d5627b24b3dcd24c63d059f4
Elliot Hillary <ehillary@integradev.com.au>
2023-08-24 10:12:47 +1000

    Remove end years from IAD license headers

--- delphi-frontend/src/main/java/au/com/integradev/delphi/type/factory/AnsiStringTypeImpl.java
+++ delphi-frontend/src/main/java/au/com/integradev/delphi/type/factory/AnsiStringTypeImpl.java
@@ -1,6 +1,6 @@
 /*
  * Sonar Delphi Plugin
- * Copyright (C) 2019-2022 Integrated Application Development
+ * Copyright (C) 2019 Integrated Application Development
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
