ade398f81b8b239fcf02bdaa04544ff73735a237
Elliot Hillary <ehillary@integradev.com.au>
2023-10-10 09:53:00 +1100

    Remove `attributeSuffix` property from `AttributeName`

--- delphi-checks/src/test/java/au/com/integradev/delphi/checks/AttributeNameCheckTest.java
+++ delphi-checks/src/test/java/au/com/integradev/delphi/checks/AttributeNameCheckTest.java
@@ -21,22 +21,12 @@ package au.com.integradev.delphi.checks;
 import au.com.integradev.delphi.builders.DelphiTestUnitBuilder;
 import au.com.integradev.delphi.checks.verifier.CheckVerifier;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.sonar.plugins.communitydelphi.api.check.DelphiCheck;
 
 class AttributeNameCheckTest {
-  private static DelphiCheck createCheck(String setting) {
-    AttributeNameCheck check = new AttributeNameCheck();
-    check.attributeSuffix = setting;
-    return check;
-  }
-
-  @ParameterizedTest
-  @ValueSource(strings = {"allowed", "required"})
-  void testAllowedOrRequiredAttributeTypeWithSuffixShouldNotAddIssue(String setting) {
+  @Test
+  void testAttributeTypeWithSuffixShouldNotAddIssue() {
     CheckVerifier.newVerifier()
-        .withCheck(createCheck(setting))
+        .withCheck(new AttributeNameCheck())
         .onFile(
             new DelphiTestUnitBuilder()
                 .appendDecl("type")
@@ -45,23 +35,10 @@ class AttributeNameCheckTest {
         .verifyNoIssues();
   }
 
-  @ParameterizedTest
-  @ValueSource(strings = {"allowed", "forbidden"})
-  void testAllowedOrForbiddenAttributeTypeWithoutSuffixShouldNotAddIssue(String setting) {
-    CheckVerifier.newVerifier()
-        .withCheck(createCheck(setting))
-        .onFile(
-            new DelphiTestUnitBuilder()
-                .appendDecl("type")
-                .appendDecl("  Foo = class(TCustomAttribute)")
-                .appendDecl("  end;"))
-        .verifyNoIssues();
-  }
-
   @Test
-  void testRequiredAttributeTypeWithoutSuffixShouldAddIssue() {
+  void testAttributeTypeWithoutSuffixShouldAddIssue() {
     CheckVerifier.newVerifier()
-        .withCheck(createCheck("required"))
+        .withCheck(new AttributeNameCheck())
         .onFile(
             new DelphiTestUnitBuilder()
                 .appendDecl("type")
@@ -71,22 +48,21 @@ class AttributeNameCheckTest {
   }
 
   @Test
-  void testForbiddenAttributeTypeWithSuffixShouldAddIssue() {
+  void testAttributeTypeWithLowercaseSuffixShouldAddIssue() {
     CheckVerifier.newVerifier()
-        .withCheck(createCheck("forbidden"))
+        .withCheck(new AttributeNameCheck())
         .onFile(
             new DelphiTestUnitBuilder()
                 .appendDecl("type")
-                .appendDecl("  FooAttribute = class(TCustomAttribute)")
+                .appendDecl("  Fooattribute = class(TCustomAttribute)")
                 .appendDecl("  end;"))
         .verifyIssueOnLine(6);
   }
 
-  @ParameterizedTest
-  @ValueSource(strings = {"allowed", "required", "forbidden"})
-  void testNonPascalCaseAttributeTypeShouldAddIssue(String setting) {
+  @Test
+  void testNonPascalCaseAttributeTypeShouldAddIssue() {
     CheckVerifier.newVerifier()
-        .withCheck(createCheck(setting))
+        .withCheck(new AttributeNameCheck())
         .onFile(
             new DelphiTestUnitBuilder()
                 .appendDecl("type")
