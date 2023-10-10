c04f1e64ef2ca6b59c2a9dad2026befbbf9f9cb3
Elliot Hillary <ehillary@integradev.com.au>
2023-10-10 16:48:51 +1100

    Include methods in `UnitLevelKeywordIndentation`

--- delphi-checks/src/test/resources/au/com/integradev/delphi/checks/UnitLevelKeywordIndentationCheck/Correct.pas
+++ delphi-checks/src/test/resources/au/com/integradev/delphi/checks/UnitLevelKeywordIndentationCheck/Correct.pas
@@ -10,6 +10,8 @@ type
   TMyObject = class(TObject)
   end;
 
+procedure MyProc;
+
 implementation
 
 uses
@@ -17,8 +19,20 @@ uses
   Math;
 
 type
-  TMyObject = class(TObject)
+  TMyOtherObject = class(TObject)
+  private
+    class procedure MyClassProc;
   end;
+
+class procedure TMyOtherObject.MyClassProc;
+begin
+  Writeln('Hello world');
+end;
+
+procedure MyProc;
+begin
+  Writeln('Hello world');
+end;
 
 initialization
 
