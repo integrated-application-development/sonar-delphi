b8d34b77ba03c59f660aff5adcad4c9c5d2e1ea6
Jonah Jeleniewski <jjeleniewski@integradev.com.au>
2023-10-20 13:05:30 +1100

    Remove `MetadataResourcePath::repository` method

--- delphi-frontend/src/main/java/org/sonar/plugins/communitydelphi/api/check/MetadataResourcePath.java
+++ delphi-frontend/src/main/java/org/sonar/plugins/communitydelphi/api/check/MetadataResourcePath.java
@@ -18,7 +18,6 @@
  */
 package org.sonar.plugins.communitydelphi.api.check;
 
-import au.com.integradev.delphi.check.MetadataResourcePathImpl;
 import org.sonar.api.scanner.ScannerSide;
 import org.sonar.api.server.ServerSide;
 import org.sonarsource.api.sonarlint.SonarLintSide;
@@ -28,8 +27,4 @@ import org.sonarsource.api.sonarlint.SonarLintSide;
 @SonarLintSide
 public interface MetadataResourcePath {
   String forRepository(String repositoryKey);
-
-  static String repository(String repositoryKey) {
-    return new MetadataResourcePathImpl().forRepository(repositoryKey);
-  }
 }
