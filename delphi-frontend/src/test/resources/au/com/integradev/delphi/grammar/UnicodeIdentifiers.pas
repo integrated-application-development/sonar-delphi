4aaa459c63c5cbedb94f76aae3e491a358d64c59
Zane Duffield <zduffield@integradev.com.au>
2023-12-11 13:07:09 +1100

    Permit more unicode codepoints in identifiers

---
+++ delphi-frontend/src/test/resources/au/com/integradev/delphi/grammar/UnicodeIdentifiers.pas
@@ -0,0 +1,29 @@
+unit UnicodeIdentifiers;
+
+interface
+
+const
+  // technically not valid Delphi, but there's no reason we shouldn't parse it properly
+  😎SurrogatePair😎 = 0;
+
+  Âçcèñtëd = 0;
+
+
+  Ĉ̵̮̓͊u̸̢̻̚ŕ̷̬͔s̴̯̰͈͗̎̾ê̷̗̚d̵̤͍͊̇͛ͅ = 0;
+
+
+  // Fullwidth numerals
+  A０ = 0;
+  A１ = 0;
+  A２ = 0;
+  A３ = 0;
+  A４ = 0;
+  A５ = 0;
+  A６ = 0;
+  A７ = 0;
+  A８ = 0;
+  A９ = 0;
+
+implementation
+
+end.
