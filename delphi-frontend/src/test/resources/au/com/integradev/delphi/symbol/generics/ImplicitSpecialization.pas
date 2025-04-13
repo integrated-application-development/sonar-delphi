b9b8c552a934ef3e17a262456d41152b4577eb42
Jonah Jeleniewski <jjeleniewski@integradev.com.au>
2025-04-14 09:44:48 +1000

    Fix implicit generic specialization of open array types
    Previously, our implicit specialization required **exact** mappings of
    types to top-level type parameters, but open arrays can go one level
    deep with the type parameter as the element type.

--- delphi-frontend/src/test/resources/au/com/integradev/delphi/symbol/generics/ImplicitSpecialization.pas
+++ delphi-frontend/src/test/resources/au/com/integradev/delphi/symbol/generics/ImplicitSpecialization.pas
@@ -4,14 +4,16 @@ interface
 
 type
   TConsumable = class
-    constructor Create; override;
+    constructor Create;
     procedure GetEaten;
     class function Pizza: TConsumable;
   end;
 
   TConsumer = class
-    procedure Test(Consumable: TConsumable);
-    procedure Consume<T: TConsumable>(Tasty: T);
+    procedure Test(Consumable: TConsumable); overload;
+    procedure Test(Consumables: array of TConsumable); overload;
+    procedure Consume<T: TConsumable>(Consumable: T); overload;
+    procedure Consume<T: TConsumable>(Consumables: array of T); overload;
   end;
 
 implementation
@@ -21,11 +23,31 @@ begin
   Consumable.GetEaten;
 end;
 
+procedure TConsumer.Consume<T>(Consumables: array of T);
+var
+  Consumable: T;
+begin
+  for Consumable in Consumables do begin
+    Consume(Consumable);
+  end;
+end;
+
 procedure TConsumer.Test(Consumable: TConsumable);
 begin
   Consume(Consumable);
   Consume(TConsumable.Pizza);
   Consume(TConsumable.Create);
+end;
+
+procedure TConsumer.Test(Consumables: array of TConsumable);
+var
+  DynamicConsumables: TArray<TConsumable>;
+begin
+  DynamicConsumables := [TConsumable.Create];
+
+  Consume(Consumables);
+  Consume(DynamicConsumables);
+  Consume([TConsumable.Pizza]);
 end;
 
 end.
