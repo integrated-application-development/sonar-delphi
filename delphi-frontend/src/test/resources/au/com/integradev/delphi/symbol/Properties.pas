46846b72c879a6fe302a6e84093563cd077d1f7b
Elliot Hillary <ehillary@integradev.com.au>
2025-05-07 15:56:36 +1000

    Disambiguate index property read/write methods correctly
    When a property has an `index` specifier, its `read` specifier must be
    a function with an `Integer` final argument, and its `write` specifier
    must be a procedure with an `Integer` second-last argument. We were
    not previously modelling this behaviour, causing `read` and `write`
    specifier methods to fail to resolve when an `index` property is present.

---
+++ delphi-frontend/src/test/resources/au/com/integradev/delphi/symbol/Properties.pas
@@ -0,0 +1,37 @@
+unit Properties;
+
+interface
+
+type
+  TIntAlias = Integer;
+
+  TMyObj = class(TObject)
+  private
+    FField: Integer;
+
+    procedure SetMyFieldConst(const Val: Integer);
+    procedure SetMyFieldAlias(Val: TIntAlias);
+    procedure SetMyField(Val: Integer);
+
+    function GetMyField: Integer;
+    function GetMyFieldAlias: TIntAlias;
+  public
+    property Prop1: Integer read FField write FField;
+    property Prop2: Integer read GetMyField write SetMyField;
+    property Prop3: Integer read GetMyField write SetMyFieldConst;
+    property Prop4: Integer read GetMyFieldAlias write SetMyFieldAlias;
+  end;
+
+  TIndexObj = class(TObject)
+  private
+    FField: TObject;
+
+    function GetMyField(Index: Integer): TObject;
+    procedure SetMyField(Index: Integer; Value: TObject);
+  public
+    property Prop1: TObject index 0 read GetMyField write SetMyField;
+  end;
+
+implementation
+
+end.
